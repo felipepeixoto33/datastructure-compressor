9728dc0ff861930d4de9df9e7272381c4e23cdb0
rvbarross <rvbarross@gmail.com>
2021-05-24 21:03:29 -0300

    leitor de arquivo txt

---
+++ compressor/src/unifor/programming/No.java
@@ -0,0 +1,11 @@
+package unifor.programming;
+
+public class No {
+    Object dado;
+    No proximo;
+
+    public No(Object dado){
+        this.dado = dado;
+        this.proximo = null;
+    }
+}
